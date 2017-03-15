67ce6b8cc22fe44cbd79640bf996e21a8c4879e6
AZIZA <AZIZA@DESKTOP-S3EUOHQ>
2017-03-15 16:37:03 +0100

    1884

---
+++ src/Test2.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AZIZA
+ */
+public class Test2 {
+    static String Drug_name,ID,Dosage,Quantity;
+
+    public static String getDrug_name() {
+        return Drug_name;
+    }
+
+    public static void setDrug_name(String Drug_name) {
+        Test2.Drug_name = Drug_name;
+    }
+
+    public static String getID() {
+        return ID;
+    }
+
+    public static void setID(String ID) {
+        Test2.ID = ID;
+    }
+
+    public static String getDosage() {
+        return Dosage;
+    }
+
+    public static void setDosage(String Dosage) {
+        Test2.Dosage = Dosage;
+    }
+
+    public static String getQuantity() {
+        return Quantity;
+    }
+
+    public static void setQuantity(String Quantity) {
+        Test2.Quantity = Quantity;
+    }
+    
+}
